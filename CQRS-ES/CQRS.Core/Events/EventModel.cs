c2c5701293438661523e94a6a7de30e029b53b28
dobre <dobrez@greentube.com>
2023-11-09 22:54:01 +0100

    added EventModel

---
+++ CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -0,0 +1,22 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CQRS.Core.Events
+{
+    /*
+    The purpose of the event model is to represent the schema of the event store and each instance of the event model 
+    will represent a record in the event store or more accurately. 
+    */
+    public class EventModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Guid AggregateIdentifier { get; set; }
+        public string AggregateType { get; set; }
+        public int Version { get; set; }
+        public string EventType { get; set; }
+        public BaseEvent EventData { get; set; }
+    }
+}
